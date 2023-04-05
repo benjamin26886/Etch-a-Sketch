baf068fcd9c6183915e509c3499255370a0c9379
benjamin26886 <73971035+benjamin26886@users.noreply.github.com>
2023-04-05 11:26:23 +0200

    Add Input field to accept user input to change the number and size of the grid
    Changed the number and size of gird using the input field, This was done by using javascript to create the field
    then adding an event listner to the field, after adding a keydown event and specifiying the enter key I had the field store
    the user's input in a variable to be used for later.
    
    close #2
    However after adding this feature there are some performace issues.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 grid = document.querySelector('.container'); // connect grid var in js to div in html
+inputData = ""; //This varibale will be used to control the size of the grid
 createGrid();
 changeBackground();
 
-// The  function below creates squares to be appended to the container 
+//let number_of_cells = getUserInput(); // This determines the number of
+
+// The function below creates squares to be appended to the container (defualt)
 function createGrid(){
     for(let i = 0; i < 256; i++){ //logic error en
         const square = document.createElement('div');
         grid.appendChild(square);
         square.classList.add('square');
+        changeBackground(square);
     }
 }
-function changeBackground(){
+function changeBackground(square){
     const squares = document.querySelectorAll('div.square'); // Select all divs with the class name of square
     squares.forEach(function(square){ // 
         square.addEventListener("mouseover", function(event){
@@ -19,5 +23,55 @@ function changeBackground(){
     });
 }
 
+//This function determines the number of cells used in the grid;
+function upDateGrid(){
+    let number_of_cells = getUserInput();
+    const upDatedContainer = document.querySelector('div.container');
+    upDatedContainer.innerHTML = "";
+
+    for(let i = 0; i < number_of_cells * number_of_cells; i++){ //logic error 
+        const squares = document.createElement('div');
+        upDatedContainer.appendChild(squares);
+        squares.classList.add('square'); // after the default class is added to the newly added squares
+        changeBackground(); // add the changeBackground class so that when the newly added squares are in the container the can change the background color
+    }
+    upDatedContainer.style.gridTemplateRows = `repeat(${number_of_cells},2fr)`; // Replace the defualt value of the rows with user value
+    upDatedContainer.style.gridTemplateColumns = `repeat(${number_of_cells},2fr)`; // Replace the default value of the columns with the user value 
+}
+
+
+//Gets the users Input
+function getUserInput(){
+    const userInput1 = inputData;
+    return userInput1;
+}
+
+// below write clear button and clear function 
+controls  = document.querySelector('.controls'); //connected the controls div to controls variable //  
+clear_button = document.createElement('button'); // created the clear button
+clear_button.classList.add("clear");
+clear_button.textContent = "clear"
+controls.appendChild(clear_button);
+
+//Below an input field is created to be used later in the program
+let user_input = document.createElement('input'); //UserInput field created
+user_input.classList.add('userInput');
+controls.appendChild(user_input); //append userInput to controls container 
+user_input.addEventListener('keydown',function(event){ //When the keydown Event occurs
+    if(event.key === "Enter"){ // the keydown event is enter 
+        event.preventDefault(); // prevent form submisson 
+        inputData = user_input.value; // save the data entered into the field in this variable, which is global, at the top of the file 
+        console.log(inputData);
+        upDateGrid(); //When the Enter key is pressed the grid should be updated from the default 
+    }
+})
+
+//Logic for clearButton function
+clear_button.addEventListener("click",function(){
+    const squares = document.querySelectorAll('div.color');
+    squares.forEach(function(square){
+        square.classList.replace("color",'square');
+    })
+})
 
 
